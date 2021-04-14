0a32c63c5742b48e35d70bd521843c953db046ac
Michael Kopf <michael.kopf@sealsystems.de>
2021-04-14 11:21:55 +0200

    Use page model and MessageBox.

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,13 +1,26 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], function(Controller) {
+  "sap/ui/core/mvc/Controller",
+  "sap/m/MessageBox"
+], function(Controller, MessageBox) {
   "use strict";
 
   return Controller.extend("de.sapui5buch.demo.controller.App", {
     onInit: function() {
-      debugger;
     },
     onPress: function() {
+      var sInputValue = this.byId("idInputField").getValue();
+      MessageBox.show("Hallo " + sInputValue, {
+        icon: MessageBox.Icon.INFORMATION,
+        title: "Willkommen"
+      });
+    },
+    onSubmit: function(oEvent) {
+      console.log("Submit event", oEvent.getParameters().value);
+      var inputValue = oEvent.getParameters().value;
+      var oPageModel = this.getOwnerComponent().getModel("page");
+      // oPageModel.person = inputValue;
+      oPageModel.setProperty("/person", inputValue);
+      console.log("pageModel.person: ", oPageModel.getProperty("/person"));
 
     }
   });
--- webapp/model/models.js
+++ webapp/model/models.js
@@ -1,16 +1,21 @@
 sap.ui.define([
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/Device"
-], function (JSONModel, Device) {
-	"use strict";
+  "sap/ui/model/json/JSONModel",
+  "sap/ui/Device"
+], function(JSONModel, Device) {
+  "use strict";
 
-	return {
+  return {
 
-		createDeviceModel: function () {
-			var oModel = new JSONModel(Device);
-			oModel.setDefaultBindingMode("OneWay");
-			return oModel;
-		}
+    createDeviceModel: function() {
+      var oModel = new JSONModel(Device);
+      oModel.setDefaultBindingMode("OneWay");
+      return oModel;
+    },
+    createPageModel: function() {
+      var oModel = new JSONModel("./model/page.model.json");
+      // oModel.setDefaultBindingMode("TwoWay");
+      return oModel;
+    }
 
-	};
+  };
 });
